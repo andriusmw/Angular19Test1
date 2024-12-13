51cdb2234c57d060ce13a6655f51729b0e9e85f5
andriusmw <andriusmw@yahoo.es>
2024-12-13 09:38:51 +0100

    ngclass example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgClassComponent } from "./components/ng-class/ng-class.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgClassComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
